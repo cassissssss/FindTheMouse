f3d41b6df865b056a37722752b2a55c4356b199e
cassis <cripse2000@gmail.com>
2026-02-16 15:55:28 +0100

    Initial commit

---
+++ src/components/TheScene.vue
@@ -0,0 +1,32 @@
+<script setup>
+  import { ref } from 'vue';
+  import '../aframe/clickable.js';
+
+  import TheCameraRig from './TheCameraRig.vue';
+
+  const allAssetsLoaded = ref(false);
+  const color = ref('tomato');
+</script>
+
+<template>
+  <a-scene
+    background="color: black;"
+  >
+
+    <a-assets @loaded="allAssetsLoaded = true">
+
+    </a-assets>
+
+    <template v-if="allAssetsLoaded">
+      <a-box
+        position="0 0 -5"
+        :color="color"
+        clickable
+        @click="color = 'blue'"
+      ></a-box>
+    </template>
+
+    <TheCameraRig />
+
+  </a-scene>
+</template>
